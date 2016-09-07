e1e2dd52d5643862ba47b65b70de01a89472ea47
Dustin John Pfister <dustin.pfister@gmail.com>
2016-09-07 15:44:10 -0400

    added boundingBox to api.js

--- src/api.js
+++ src/api.js
@@ -6,6 +6,22 @@ var api = {
         // return the distance between the two points
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 
-    }
+    },
+
+    // your basic bounding box collision detection
+    boundingBox : function (x1, y1, w1, h1, x2, y2, w2, h2) {
+
+        // if the two objects do not overlap
+        if ((x1 > x2 + w2) || (x1 + w1 < x2) || (y1 + h1 < y2) || (y1 > y2 + h2)) {
+
+            //then they do not overlap
+            return false;
+
+        }
+
+        // else they do
+        return true;
+
+    },
 
 };
