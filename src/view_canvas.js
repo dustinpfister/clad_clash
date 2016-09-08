e4675e132f307f12afb5d74c08fd4d191f72238c
Dustin John Pfister <dustin.pfister@gmail.com>
2016-09-08 11:38:07 -0400

    copyed and pased draw methods from old view, and drawing the map.

--- src/view_canvas.js
+++ src/view_canvas.js
@@ -12,17 +12,86 @@ var View = (function () {
     ctx = canvas.getContext('2d'),
     container,
 
+    drawMap = function () {
+
+        var cells = Map.cells,
+        width = Map.conf.width,
+        offset = Map.conf.offset,
+        i = 0,
+        len = cells.length,
+        x,
+        y;
+
+        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+        while (i < len) {
+
+            x = i % width;
+            y = Math.floor(i / width);
+
+            ctx.fillStyle = '#ffff00';
+            if (Map.cells[i].water) {
+                ctx.fillStyle = '#00ffff';
+            }
+            ctx.fillRect(x * 32 + offset.x, y * 32 + offset.y, 32, 32);
+            ctx.strokeRect(x * 32 + offset.x, y * 32 + offset.y, 32, 32);
+
+            if (Map.cells[i].movePoint) {
+
+                ctx.beginPath();
+                ctx.arc(x * 32 + 16 + offset.x, y * 32 + 16 + offset.y, 10, 0, Math.PI * 2);
+                ctx.stroke();
+            }
+
+            i++;
+        }
+
+    },
+
+    drawBoats = function () {
+
+        var boats = BoatCollection.boats,
+        i = 0,
+        offset = Map.conf.offset,
+        len = boats.length,
+        x,
+        y;
+
+        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+        while (i < len) {
+
+            ctx.fillStyle = boats[i].owner === 'player' ? '#00ff00' : '#ff0000';
+            ctx.fillRect(boats[i].x * 32 + offset.x, boats[i].y * 32 + offset.y, 32, 32);
+
+            i++;
+        }
+
+        if (BoatCollection.selected) {
+
+            ctx.beginPath();
+            ctx.arc(
+                boats[BoatCollection.selected - 1].x * 32 + 16 + offset.x,
+                boats[BoatCollection.selected - 1].y * 32 + 16 + offset.y,
+                boats[BoatCollection.selected - 1].movement * 32 + 16,
+                0,
+                Math.PI * 2);
+            ctx.stroke();
+
+        }
+
+    },
+
     draw = {
 
         start : function () {},
 
         game : function () {
+
+            ctx.fillStyle = '#8a8a8a';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
 			
-			ctx.fillStyle = '#8a8a8a';
-			ctx.fillRect(0,0,canvas.width,canvas.height)
-			
-			
-		}
+			drawMap();
+
+        }
 
     }
 
@@ -32,9 +101,9 @@ var View = (function () {
         setup : function (id) {
 
             console.log('setting up view');
-			
-			canvas.width = 800;
-			canvas.height = 600;
+
+            canvas.width = 800;
+            canvas.height = 600;
 
             container = document.getElementById(id);
             container.appendChild(canvas);
@@ -42,10 +111,10 @@ var View = (function () {
         },
 
         draw : function (state) {
-			
-			draw[state]();
-			
-		}
+
+            draw[state]();
+
+        }
 
     }
 
