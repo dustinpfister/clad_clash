f1b57bebd0b7f5d775a7252ab74401e74e0381a3
Dustin John Pfister <dustin.pfister@gmail.com>
2016-09-08 11:47:53 -0400

    removed old unused code in main.js

--- src/main.js
+++ src/main.js
@@ -67,26 +67,3 @@
 
 }
     ());
-
-/*
-var canvas = document.getElementsByTagName('canvas')[0],
-context = canvas.getContext('2d');
-
-attach(canvas);
-drawGrid(canvas, context);
-drawBoats(canvas, context);
-
-var loop = function () {
-
-requestAnimationFrame(loop);
-
-context.clearRect(0, 0, canvas.width, canvas.height);
-
-drawBack(canvas, context);
-drawGrid(canvas, context);
-drawBoats(canvas, context);
-
-};
-
-loop();
-*/
