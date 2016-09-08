3709b6202ba9f99344d63664e058264aff290218
Dustin John Pfister <dustin.pfister@gmail.com>
2016-09-08 13:25:06 -0400

    AI first action

--- src/ai.js
+++ src/ai.js
@@ -2,14 +2,34 @@
 
 var AI = (function () {
 
+    var firstAction = true,
+    action = false,
+    aiBoats;
+
     return {
 
         // what to do when it is the AI's turn
         tick : function () {
 
-		    console.log('i am AI');
-		
-            Game.AIOver();
+            // if no action is in progress
+            if (!action) {
+
+                action = true;
+
+                // first action?
+                if (firstAction) {
+
+                    firstAction = false;
+
+                    console.log('AI: finding stuff for my first action.');
+
+                    aiBoats = BoatCollection.getBoatsByOwner('ai');
+
+                }
+
+            }
+
+            //Game.AIOver();
 
         }
 
